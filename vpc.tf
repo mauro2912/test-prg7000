f1c87687cd081cd3891694a8009d57ba23211f38
mauro2912 <asanchez2912@gmail.com>
2023-03-07 10:05:08 -0500

    Comment vpc flow_log section vpc.tf file - andres.gonzalez

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_flow_log" "flow-logs-vpc-challenge1" {
   #  log_group_name = aws_cloudwatch_log_group.logsGroup-challenge1
-  iam_role_arn = aws_iam_role.flow-logs-role.id
-  vpc_id       = aws_vpc.vpc-challenge1.id
+  #iam_role_arn = aws_iam_role.flow-logs-role.id
+  vpc_id = aws_vpc.vpc-challenge1.id
 }
 
 resource "aws_vpc" "vpc-challenge1" {
